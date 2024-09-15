77cc7b698463e98b0c2ee8e347f6d2633bc73c99
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-09-14 21:35:01 -0300

    comentarios

--- interatividade/script.js
+++ interatividade/script.js
@@ -1,51 +1,58 @@
 function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if(fano.value.length == 0 || Number(fano.value) > ano) {
+    var data = new Date() // Obtém a data atual
+    var ano = data.getFullYear() // Obtém o ano atual
+    var fano = document.getElementById('txtano') // Obtém o elemento de input do ano de nascimento
+    var res = document.querySelector('div#res') // Obtém a div onde será exibido o resultado
+
+    // Verifica se o campo do ano de nascimento está vazio ou se o ano inserido é maior que o ano atual
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var gênero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        img.style.margin = '15px'
+        var fsex = document.getElementsByName('radsex') // Obtém a lista de elementos de radio buttons com o nome 'radsex'
+        var idade = ano - Number(fano.value) // Calcula a idade com base no ano de nascimento fornecido
+        var gênero = '' // Variável para armazenar o gênero da pessoa
+        var img = document.createElement('img') // Cria um elemento de imagem <img>
+        img.setAttribute('id', 'foto') // Define o atributo 'id' do elemento imagem como 'foto'
+        img.style.margin = '15px' // Adiciona uma margem de 15px ao redor da imagem
 
-        if (fsex[0].checked) {
+        // Verifica qual opção de gênero foi selecionada e define a imagem correspondente à idade
+        if (fsex[0].checked) { // Se o radio button 'Homem' estiver selecionado
             gênero = 'Homem'
             if (idade >= 0 && idade < 10) {
-                //crianca
-                img.setAttribute('src', 'imagens/menino.png')
+                // Criança
+                img.setAttribute('src', 'imagens/menino.png') // Imagem para uma criança do sexo masculino
             } else if (idade < 21) {
-                //jovem
-                img.setAttribute('src', 'imagens/rapaz.png')
+                // Jovem
+                img.setAttribute('src', 'imagens/rapaz.png') // Imagem para um jovem do sexo masculino
             } else if (idade < 50) {
-                //adulto
-                img.setAttribute('src', 'imagens/homem.png')
+                // Adulto
+                img.setAttribute('src', 'imagens/homem.png') // Imagem para um adulto do sexo masculino
             } else {
-                //idoso
-                img.setAttribute('src', 'imagens/idoso.png')
+                // Idoso
+                img.setAttribute('src', 'imagens/idoso.png') // Imagem para um idoso do sexo masculino
             }
-        } else if (fsex[1].checked) {
+        } else if (fsex[1].checked) { // Se o radio button 'Mulher' estiver selecionado
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
-                //crianca
-                img.setAttribute('src', 'imagens/menina.png')
+                // Criança
+                img.setAttribute('src', 'imagens/menina.png') // Imagem para uma criança do sexo feminino
             } else if (idade < 21) {
-                //jovem
-                img.setAttribute('src', 'imagens/moca.png')
+                // Jovem
+                img.setAttribute('src', 'imagens/moca.png') // Imagem para uma jovem do sexo feminino
             } else if (idade < 50) {
-                //adulto
-                img.setAttribute('src', 'imagens/mulher.png')
+                // Adulta
+                img.setAttribute('src', 'imagens/mulher.png') // Imagem para uma adulta do sexo feminino
             } else {
-                //idoso
-                img.setAttribute('src', 'imagens/idosa.png')
+                // Idosa
+                img.setAttribute('src', 'imagens/idosa.png') // Imagem para uma idosa do sexo feminino
             }
         }
+
+        // Alinha o texto da div 'res' ao centro
         res.style.textAlign = 'center'
+        // Define o conteúdo da div 'res' com o gênero e a idade detectados
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        // Adiciona a imagem correspondente à idade e gênero à div 'res'
         res.appendChild(img)
     }
 }
